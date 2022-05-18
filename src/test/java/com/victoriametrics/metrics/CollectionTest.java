f514ec41808c7487202e7439b883df0e208aa81d
Valery Kantor <valery.kantor@gmail.com>
2022-05-18 12:14:24 +0200

    Collection metrics facade.

---
+++ src/test/java/com/victoriametrics/metrics/CollectionTest.java
@@ -0,0 +1,120 @@
+package com.victoriametrics.metrics;
+
+import com.victoriametrics.validator.InvalidMetricNameException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CollectionTest {
+
+    @Test
+    public void createCounterMetricWithBuilder() {
+        Collection collection = Collection.create();
+        Counter counterNoLabels = collection.createCounter()
+                .name("foo")
+                .then()
+                .register();
+
+        assertEquals("foo", counterNoLabels.getName());
+
+        Counter counterWithLabels = collection.createCounter()
+                .name("foo")
+                .addLabel("bar", "value1")
+                .then()
+                .register();
+
+        assertEquals("foo{bar=\"value1\"}", counterWithLabels.getName());
+    }
+
+    @Test
+    public void createGaugeMetricWithBuilder() {
+        Collection collection = Collection.create();
+        Gauge gaugeNoLabels = collection.createGauge()
+                .withSupplier(() -> 1.0)
+                .name("foo")
+                .then()
+                .register();
+
+        assertEquals("foo", gaugeNoLabels.getName());
+
+        Gauge gaugeWithLabels = collection.createGauge()
+                .withSupplier(() -> 1.0)
+                .name("foo")
+                .addLabel("bar", "value1")
+                .then()
+                .register();
+
+        assertEquals("foo{bar=\"value1\"}", gaugeWithLabels.getName());
+    }
+
+    @Test
+    public void createHistogramMetricWithBuilder() {
+        Collection collection = Collection.create();
+        Histogram histogramNoLabels = collection.createHistogram()
+                .name("foo")
+                .then()
+                .register();
+
+        assertEquals("foo", histogramNoLabels.getName());
+
+        Histogram histogramWithLabels = collection.createHistogram()
+                .name("foo")
+                .addLabel("bar", "value1")
+                .then()
+                .register();
+
+        assertEquals("foo{bar=\"value1\"}", histogramWithLabels.getName());
+    }
+
+    @Test
+    public void createMetricsByName() {
+        Collection collection = Collection.create();
+        Counter counter = collection.getOrCreateCounter("foo");
+        Gauge gauge = collection.getOrCreateGauge("foo{bar=\"value1\"}", () -> 2.0);
+        Histogram histogram = collection.getOrCreateHistogram("foo{bar=\"value1\", baz=\"value2\"}");
+
+        assertEquals(3, collection.size());
+        assertEquals("foo", counter.getName());
+        assertEquals("foo{bar=\"value1\"}", gauge.getName());
+        assertEquals("foo{bar=\"value1\", baz=\"value2\"}", histogram.getName());
+    }
+
+    @Test
+    public void createMetricWithInvalidName_thenThrowException() {
+        Collection collection = Collection.create();
+        assertThrowsExactly(InvalidMetricNameException.class, () -> collection.getOrCreateHistogram("foo{"));
+        assertThrowsExactly(InvalidMetricNameException.class, () -> collection.getOrCreateHistogram("foo{label=}"));
+    }
+
+    @Test
+    public void testGetOrCreateMetric() {
+        Collection collection = Collection.create();
+        Counter counter1 = collection.getOrCreateCounter("foo");
+        Counter counter2 = collection.getOrCreateCounter("foo");
+        Counter counter3 = collection.getOrCreateCounter("foo");
+
+
+        assertEquals(counter1, counter2);
+        assertEquals(counter1, counter3);
+        assertEquals(counter2, counter3);
+        assertEquals(1, collection.size());
+
+        counter1.inc();
+        counter2.inc();
+
+        assertEquals(2, counter1.get());
+        assertEquals(2, counter2.get());
+        assertEquals(2, counter3.get());
+    }
+
+    @Test
+    public void tesGetMetricByName() {
+        Collection collection = Collection.create();
+        collection.getOrCreateCounter("foo");
+        Counter counter = collection.getMetric("foo");
+        assertNotNull(counter);
+
+        Gauge gauge = collection.getMetric("bar");
+        assertNull(gauge);
+    }
+}
